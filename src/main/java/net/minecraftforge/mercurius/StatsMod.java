3b6037fed21adcb6e8d264841b6c830b7889ad70
TamasHenning <tamas@henning.ws>
2016-08-29 18:25:49 -0700

    One more name change

--- src/main/java/net/minecraftforge/mercurius/StatsMod.java
+++ src/main/java/net/minecraftforge/mercurius/StatsMod.java
@@ -12,8 +12,8 @@ import net.minecraftforge.mercurius.utils.sender.CommonSender;
 @Mod(modid = StatsMod.MODID, name = StatsMod.MODNAME,  version = StatsMod.VERSION, guiFactory = StatsMod.GUIFACTORY)
 public class StatsMod
 {
-    public static final String MODID = "forgestats";
-    public static final String MODNAME = "Forge Stats";
+    public static final String MODID = "mercurius";
+    public static final String MODNAME = "Mercurius";
     public static final String VERSION = "1.0.0";
     public static final String GUIFACTORY = "net.minecraftforge.mercurius.gui.GuiFactory";
 
